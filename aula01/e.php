afc38abf8d55e5dac5cdad731f614915a8ce6826
sandaele <sandalia207@gmail.com>
2025-03-26 17:50:42 -0300

    progresso

---
+++ aula01/e.php
@@ -0,0 +1,5 @@
+<?php
+/* e) Escreva um algoritmo que leia a altura e o peso de uma pessoa, calcule o seu IMC 
+(Índice de Massa Corporal) e exiba para o usuário
+*/
+?>
---
+++ aula01/f.php
@@ -0,0 +1,5 @@
+<?php
+/* f) Escreva um algoritmo que receba o valor de um produto e calcule um desconto de 7%, 
+exibindo para o usuário o valor original, o valor do desconto e o valor com o desconto
+*/
+?>
