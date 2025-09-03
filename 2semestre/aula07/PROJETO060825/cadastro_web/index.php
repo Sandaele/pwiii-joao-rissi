ded6ea0d7840a97e6d6700680c3d509e011bf53b
sandaele <sandalia207@gmail.com>
2025-09-03 17:04:49 -0300

    socorro

--- 2semestre/aula07/PROJETO060825/cadastro_web/index.php
+++ 2semestre/aula07/PROJETO060825/cadastro_web/index.php
@@ -48,18 +48,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro Web</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>CAD-WEB</h1>
-    
-    <style>
-    #cadastro-container {
-        display: none; /* começa invisível */
-    }
-    </style>
 
     <div>
     <button type="button" onclick="toggleCadastro()">Cadastro</button>
--- 2semestre/aula07/PROJETO060825/cadastro_web/veiculos_db.sql
+++ 2semestre/aula07/PROJETO060825/cadastro_web/veiculos_db.sql
@@ -4,11 +4,11 @@ use veiculos_db;
 create table if not exists automovel (
     id int auto_increment not null,
     modelo varchar(30) not null,
-    ano int(4) not null,
+    ano int(4),
     placa varchar(8) not null,
     data_cadastro date not null,
-    cor varchar(15) not null,
-    valor double(10, 2) not null,
+    cor varchar(15),
+    valor double(10,2),
     unique(placa),
     primary key(id)
-)
+);
